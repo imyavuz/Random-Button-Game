15d32ed94a7dc995885838a4e6403b7d0e02715f
Kürşat Yavuz YILMAZ <147814471+imyavuz@users.noreply.github.com>
2024-11-15 02:19:26 +0300

    Add files via upload

---
+++ Rastgele_Buton/Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿
+namespace Rastgele_Buton
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnStartStop = new System.Windows.Forms.Button();
+            this.btnCiftRenk = new System.Windows.Forms.Button();
+            this.txtRenkKodu = new System.Windows.Forms.TextBox();
+            this.btnCikis = new System.Windows.Forms.Button();
+            this.btnTemizle = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnStartStop
+            // 
+            this.btnStartStop.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnStartStop.Location = new System.Drawing.Point(9, 312);
+            this.btnStartStop.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnStartStop.Name = "btnStartStop";
+            this.btnStartStop.Size = new System.Drawing.Size(157, 44);
+            this.btnStartStop.TabIndex = 1;
+            this.btnStartStop.Text = "Rastgele Başlat";
+            this.btnStartStop.UseVisualStyleBackColor = true;
+            this.btnStartStop.Click += new System.EventHandler(this.btnStartStop_Click);
+            // 
+            // btnCiftRenk
+            // 
+            this.btnCiftRenk.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnCiftRenk.Location = new System.Drawing.Point(170, 312);
+            this.btnCiftRenk.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnCiftRenk.Name = "btnCiftRenk";
+            this.btnCiftRenk.Size = new System.Drawing.Size(157, 44);
+            this.btnCiftRenk.TabIndex = 3;
+            this.btnCiftRenk.Text = "Kırmızı-Mavi Başlat";
+            this.btnCiftRenk.UseVisualStyleBackColor = true;
+            this.btnCiftRenk.Click += new System.EventHandler(this.btnCiftRenk_Click);
+            // 
+            // txtRenkKodu
+            // 
+            this.txtRenkKodu.BackColor = System.Drawing.Color.Black;
+            this.txtRenkKodu.Enabled = false;
+            this.txtRenkKodu.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtRenkKodu.ForeColor = System.Drawing.Color.Transparent;
+            this.txtRenkKodu.Location = new System.Drawing.Point(9, 10);
+            this.txtRenkKodu.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtRenkKodu.Name = "txtRenkKodu";
+            this.txtRenkKodu.Size = new System.Drawing.Size(186, 30);
+            this.txtRenkKodu.TabIndex = 4;
+            this.txtRenkKodu.Text = "Buton Seçilmedi";
+            // 
+            // btnCikis
+            // 
+            this.btnCikis.BackColor = System.Drawing.Color.Red;
+            this.btnCikis.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnCikis.Location = new System.Drawing.Point(506, 312);
+            this.btnCikis.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnCikis.Name = "btnCikis";
+            this.btnCikis.Size = new System.Drawing.Size(85, 44);
+            this.btnCikis.TabIndex = 5;
+            this.btnCikis.Text = "ÇIKIŞ";
+            this.btnCikis.UseVisualStyleBackColor = false;
+            this.btnCikis.Click += new System.EventHandler(this.btnCikis_Click);
+            // 
+            // btnTemizle
+            // 
+            this.btnTemizle.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnTemizle.Location = new System.Drawing.Point(380, 312);
+            this.btnTemizle.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnTemizle.Name = "btnTemizle";
+            this.btnTemizle.Size = new System.Drawing.Size(122, 44);
+            this.btnTemizle.TabIndex = 6;
+            this.btnTemizle.Text = "Temizle";
+            this.btnTemizle.UseVisualStyleBackColor = true;
+            this.btnTemizle.Click += new System.EventHandler(this.btnTemizle_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(600, 366);
+            this.Controls.Add(this.btnTemizle);
+            this.Controls.Add(this.btnCikis);
+            this.Controls.Add(this.txtRenkKodu);
+            this.Controls.Add(this.btnCiftRenk);
+            this.Controls.Add(this.btnStartStop);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Rastgele Buton Oluşturma";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStartStop;
+        private System.Windows.Forms.Button btnCiftRenk;
+        private System.Windows.Forms.TextBox txtRenkKodu;
+        private System.Windows.Forms.Button btnCikis;
+        private System.Windows.Forms.Button btnTemizle;
+    }
+}
+
---
+++ Rastgele_Buton/Form1.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Rastgele_Buton
+{
+    public partial class Form1 : Form
+    {
+        private Timer timerRandom;
+        private Timer timerAlternating;
+        private Random random;
+        private bool isRandomCreating;
+        private bool isAlternatingCreating;
+        private bool isRed;
+        public Form1()
+        {
+            random = new Random();
+            isRandomCreating = false;
+            isAlternatingCreating = false;
+            isRed = true;
+
+
+            timerRandom = new Timer();
+            timerRandom.Interval = 1000;
+            timerRandom.Tick += TimerRandom_Tick;
+
+
+            timerAlternating = new Timer();
+            timerAlternating.Interval = 1000;
+            timerAlternating.Tick += TimerAlternating_Tick;
+            InitializeComponent();
+        }
+
+        private void btnStartStop_Click(object sender, EventArgs e)
+        {
+
+            if (!isRandomCreating)
+            {
+
+                timerRandom.Start();
+                isRandomCreating = true;
+                btnStartStop.Text = "Rastgele Durdur";
+            }
+            else
+            {
+
+                timerRandom.Stop();
+                isRandomCreating = false;
+                btnStartStop.Text = "Rastgele Başlat";
+            }
+        }
+
+        private void btnCiftRenk_Click(object sender, EventArgs e)
+        {
+
+            if (!isAlternatingCreating)
+            {
+
+                timerAlternating.Start();
+                isAlternatingCreating = true;
+                btnCiftRenk.Text = "Kırmızı-Mavi Durdur";
+            }
+            else
+            {
+
+                timerAlternating.Stop();
+                isAlternatingCreating = false;
+                btnCiftRenk.Text = "Kırmızı-Mavi Başlat";
+            }
+        }
+
+        private void TimerRandom_Tick(object sender, EventArgs e)
+        {
+
+            Button newButton = new Button();
+            newButton.Size = new Size(50, 50);
+
+
+            int x = random.Next(0, this.ClientSize.Width - newButton.Width);
+            int y = random.Next(0, this.ClientSize.Height - newButton.Height);
+            newButton.Location = new Point(x, y);
+
+
+            Color randomColor = Color.FromArgb(random.Next(256), random.Next(256), random.Next(256));
+            newButton.BackColor = randomColor;
+
+
+            newButton.Click += (s, args) =>
+            {
+                txtRenkKodu.Text = ColorTranslator.ToHtml(randomColor);
+            };
+
+
+            this.Controls.Add(newButton);
+        }
+
+        private void TimerAlternating_Tick(object sender, EventArgs e)
+        {
+
+            Button newButton = new Button();
+            newButton.Size = new Size(50, 50);
+
+
+            int x = random.Next(0, this.ClientSize.Width - newButton.Width);
+            int y = random.Next(0, this.ClientSize.Height - newButton.Height);
+            newButton.Location = new Point(x, y);
+
+
+            if (isRed)
+            {
+                newButton.BackColor = Color.Red;
+            }
+            else
+            {
+                newButton.BackColor = Color.Blue;
+            }
+
+
+            isRed = !isRed;
+
+
+            newButton.Click += (s, args) =>
+            {
+                txtRenkKodu.Text = ColorTranslator.ToHtml(newButton.BackColor);
+            };
+
+
+            this.Controls.Add(newButton);
+        }
+
+        private void btnCikis_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void btnTemizle_Click(object sender, EventArgs e)
+        {
+            foreach (Control control in this.Controls)
+            {
+
+                if (control is Button && control != btnStartStop && control != btnCiftRenk && control != btnTemizle && control != btnCikis)
+                {
+                    this.Controls.Remove(control);
+                    control.Dispose();
+                }
+            }
+
+
+            txtRenkKodu.Text = "Buton Seçilmedi";
+        }
+    }
+}
